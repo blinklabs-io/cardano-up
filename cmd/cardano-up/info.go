2311387cab2fac77693830a4cac84b8f3a558e04
Aurora Gaffney <aurora@blinklabs.io>
2024-03-04 21:41:30 -0600

    feat: info command (#75)
    Fixes #58

---
+++ cmd/cardano-up/info.go
@@ -0,0 +1,53 @@
+// Copyright 2024 Blink Labs Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/blinklabs-io/cardano-up/pkgmgr"
+	"github.com/spf13/cobra"
+)
+
+func infoCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:     "info",
+		Aliases: []string{"status"},
+		Short:   "Show info for an installed package",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) == 0 {
+				return errors.New("no package provided")
+			}
+			if len(args) > 1 {
+				return errors.New("only one package may be specified a a time")
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			pm, err := pkgmgr.NewDefaultPackageManager()
+			if err != nil {
+				slog.Error(fmt.Sprintf("failed to create package manager: %s", err))
+				os.Exit(1)
+			}
+			if err := pm.Info(args[0]); err != nil {
+				slog.Error(err.Error())
+				os.Exit(1)
+			}
+		},
+	}
+}
